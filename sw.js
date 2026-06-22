50d8f530e8a4cca210180c2eae8430032e1ab7e4
garajteknokrat <legendbarbershoppe@gmail.com>
2026-06-22 21:47:13 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,5 +11,5 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(fetch(event.request, { cache: 'no-store' }));
 });
